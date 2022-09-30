9c9f9f70235d34519ef5cddb217d28dc735dee48
Breno Abreu <breno2601@gmail.com>
2022-09-29 23:56:21 -0300

    Online DFS agent algorithm added

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,22 +1,16 @@
 from environment import Environment
 from file_reader import FileReader
 from a_star import AStar
+from agent_explorer import AgentExplorer
 
 def main():
     reader = FileReader()
     environment = Environment(reader.get_environment_data(), reader.get_vital_signs_data())
-
     environment.print_map()
     
-    a_star = AStar(environment.get_map())
-    path = a_star.run((0, 0), (4, 3))
-    positions = a_star.get_path_only_positions(path['path'])
-
-    environment.print_map_with_path(positions)
-    print('cost: ' + str(path['cost']))
-
-    print(positions)
-
+    explorer = AgentExplorer(environment.get_map(), environment.get_base_position(), environment.get_te(), (10, 10))
+    explorer.explore()
+    
 
 if __name__ == "__main__":
     main()
